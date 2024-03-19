73186f674649234a1174f8542b14984de2f4d8be
Rutu <123353444+rutudobariya@users.noreply.github.com>
2024-03-19 20:52:14 +0530

    Add files via upload

---
+++ prot/texttyped/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings: ["Front end Developer", "React JS Developer", "Web Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+
+})
